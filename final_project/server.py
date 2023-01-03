09ff56f3b7e69f375313ad8e2969f4d8ea466791
Huntershola <youremail@domain.com>
2023-01-03 14:23:42 +0100

    first commit

---
+++ final_project/server.py
@@ -0,0 +1,25 @@
+import machinetranslation
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    result = translator.englishtofrench(textToTranslate)
+    return "Translated text to French: " + result
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    result = translator.frenchtoenglish(textToTranslate)
+    return "Translated text to English: " + result
+
+@app.route("/")
+def renderIndexPage()
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
